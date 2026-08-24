6bd9a2d6727538e7d88f2f22db55a4f12d15d531
Tayler <taylernevespolletti@gmail.com>
2026-08-24 20:15:25 +0000

    chore: remove node_modules and .next from git tracking

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <>
         <h1>Vou ter que fazer esse desafio depois</h1>
-        <p>Continuando, isso funciona?</p>
+        <p>Continuando, isso funciona? Agora sei mexer mais</p>
     </>
 }
 
