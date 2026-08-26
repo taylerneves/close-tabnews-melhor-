395ebb442456be72f55aa07aeac67ddadb9e2c2f
Tayler <taylernevespolletti@gmail.com>
2026-08-26 19:29:31 +0000

    Fazendo a declaração para o meu amor!

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <>
-        <h1>Vou ter que fazer esse desafio depois</h1>
-        <p>Continuando, isso funciona? Agora sei mexer mais</p>
+        <h1>Isabelli, você vai conseguir! Você é a mulher da minha vida</h1>
+        <p>Estou testando novas tecnologias!</p>
     </>
 }
 
